f36c7d38af4b3824530fabedb4b1570ae565fdda
Tamamyan Eremia <eremia.tamamyan.04@mail.ru>
2024-11-23 21:29:49 +0400

    creating the carcass

---
+++ Json_parser/json_class.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <iostream>
+#include <map>
+#include <fstream>
+#include <istream>
+#include <string>
+
+class Json
+{
+public:
+	Json();
+	Json(std::string path);
+	~Json();
+
+private:
+
+}; 
+Json::Json()
+{
+}
+Json::Json(std::string path)
+{
+}
+
+Json::~Json()
+{
+}
+
